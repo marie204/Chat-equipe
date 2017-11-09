e2ccbe62c07b471f80849754aeaa091b57551ae4
Unknown <Raven4Tey@hotmail.com>
2017-11-09 14:51:13 +0100

    Lien AJAX-API

--- indexu.php
+++ indexu.php
@@ -4,9 +4,13 @@
 	<title></title>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	
+
 </head>
 <body>
 
+<script>
+var query = {
+   		url: 'http://http://messenger.api.niamor.com/
+   	</script>
 </body>
 </html>
