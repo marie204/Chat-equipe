a1fe6f9f0d54bc65820c03fcf3f96e3c0849c59a
Unknown <marreg@rocketmail.com>
2017-11-13 10:42:30 +0100

    Fonction recupMessage et recupPseudo et sendmessage working

--- indexu.php
+++ indexu.php
@@ -13,30 +13,81 @@
 <body class="lightblue">
 	<div class="row" style="width: 100vh">
 	<div class="col-9" id="chat" style="background-color: pink;">
+		<button onclick="recupChatMsg();"> recup message</button>
 	</div>
 
-	<div class="col-3 lightblue" id="pseudo" style="background-color: lightblue; height: 100vh"></div></div>
+	<div class="col-3 lightblue" id="pseudo" style="background-color: lightblue; height: 100vh">
+		<button onclick="getUsers();">Recup pseudo</button>
+	</div></div>
 
-	<footer class="lightblue"> <input class="lightblue" id="inputfoot" type="text"> <button id="buttonfoot">valider</button></footer>
+
+	<footer class="lightblue"> <input class="lightblue" id="inputfoot" type="text"> <button id="buttonfoot" onclick="sendMessage();">valider</button></footer>
 
 	<script>
-		recupChat()
+		var userInfo;
+		recupChat();
+		
 
 		function recupChat(){
 			$.ajax({
 				url:'http://messenger.api.niamor.com/createUser',
 				method:'post'				
-			}).done(afficheChat);
+			}).done(saveUser);
 		}
-		function afficheChat(user){
-			console.log(user.authKey);
+		function saveUser(user){
+			userInfo = [user.id, user.authKey, user.username, user.createdAt, user.lastMessageAt];
+			//console.log(userInfo);/*cause la mort du navigateur! for (var i = 0; i != msg.length - 1; i++) {console.log(msg[i]);}*/
+		}
+		function getUsers(){
+			$.ajax({
+				url:'http://messenger.api.niamor.com/getUsers', 
+				method:'post', 
+			}).done(affichePseudo)
+		};
+		function affichePseudo(otUsers){
+			//a = otUsers.length;
+			//02e2b95864c8a3e6b239e3871ad0b975
+			for (var i = 0; i != otUsers.length; i++) {
+				document.getElementById('pseudo').innerHTML +=  otUsers[i].username+ "<br>";
+				//console.log(otUsers[i]);
+			}
+			//console.log(a);
+		}
 
-			/*cause la mort du navigateur! for (var i = 0; i != msg.length - 1; i++) {
-				console.log(msg[i]);
-			}*/
-
-
-			
+//userInfo[1]
+		function recupChatMsg(){
+			//console.log(userInfo[1])
+			$.ajax({
+				url:'http://messenger.api.niamor.com/getMessages', 
+				method: 'post', 
+				data: {
+					authKey: userInfo[1],
+					lastId : 0,
+				}
+				
+			}).done(afficheMsg)
+		}
+		//console.log(userInfo);
+		function afficheMsg(msgEnv){
+			for (var i = 0; i <= msgEnv.length; i++) {
+				console.log(msgEnv[i])
+				document.getElementById('chat').innerHTML += msgEnv[i].from.username+":"+msgEnv[i].text+"<br>";
+			}
+		}
+		function sendMessage(){
+			a = document.getElementById('inputfoot').innerHTML;
+			$.ajax({
+				url:'http://messenger.api.niamor.com/sendMessage',
+				method:'post',
+				data:{
+					authKey: userInfo[1],
+					text: document.getElementById('inputfoot').value, 
+					to: 0
+				}
+			}).done(clearValue)
+		}
+		function clearValue(){
+			document.getElementById('inputfoot').value = '';
 		}
 	</script>
 </body>
